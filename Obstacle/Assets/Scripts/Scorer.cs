c155c8841f89f7187abfc8440a9302dc040e81ac
Hüseyin Kaymak <huseyinkaymak99@gmail.com>
2023-07-18 05:38:43 +0300

    Display score

--- Obstacle/Assets/Scripts/Scorer.cs
+++ Obstacle/Assets/Scripts/Scorer.cs
@@ -1,13 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Scorer : MonoBehaviour
 {
-    int score;
+    [SerializeField] Text scoreText;
+    int score = -1;
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag != "Hit")
+        {
             score++;
+            scoreText.text = score.ToString();
+        }
     }
 }
--- Obstacle/Assets/Scripts/Spinner.cs
+++ Obstacle/Assets/Scripts/Spinner.cs
@@ -7,11 +7,11 @@ public class Spinner : MonoBehaviour
 {
 
     [SerializeField] float xRotate = 0f;
-    [SerializeField] float yRotate = 1f;
+    [SerializeField] float yRotate = 100f;
     [SerializeField] float zRotate = 0f;
 
     void Update()
     {
-        transform.Rotate(xRotate * Time.deltaTime, yRotate, zRotate * Time.deltaTime);
+        transform.Rotate(xRotate * Time.deltaTime, yRotate * Time.deltaTime, zRotate * Time.deltaTime);
     }
 }
