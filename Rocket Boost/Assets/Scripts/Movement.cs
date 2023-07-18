c0dd085ea6c735d43ac4922973d10c6a647256b1
Hüseyin Kaymak <huseyinkaymak99@gmail.com>
2023-07-18 23:48:31 +0300

    Movement, audio

---
+++ Rocket Boost/Assets/Scripts/Movement.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] float thrust = 1000f;
+    [SerializeField] float rotationThrust = 100f;
+    Rigidbody rb;
+    AudioSource audioSource;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    void ProcessThrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.AddRelativeForce(Vector3.up * thrust * Time.deltaTime);
+
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+            else
+            {
+                audioSource.Stop();
+            }
+        }
+    }
+
+    void ProcessRotation()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            ApplyRotation(rotationThrust);
+        }
+
+        else if (Input.GetKey(KeyCode.D))
+        {
+            ApplyRotation(-rotationThrust);
+        }
+    }
+
+    void ApplyRotation(float rotation)
+    {
+        rb.freezeRotation = true;
+        transform.Rotate(Vector3.forward * rotation * Time.deltaTime);
+        rb.freezeRotation = false;
+    }
+}
